05047d05c27040171cd49877a605264c231ca57a
Léo <69589034+Zomblard@users.noreply.github.com>
2023-01-31 19:01:13 +0100

    [ADD] default font

--- dev/include/head.php
+++ dev/include/head.php
@@ -9,6 +9,29 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <script src="https://kit.fontawesome.com/eced519d56.js" crossorigin="anonymous"></script>
 
+    <!--A UTILISER UNIQUEMENT SI ON VEUT LE SWITCH LIGHT / DARK
+    <script>
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    </script>-->
 </head>
 
-<body>
+
+<!--Attention ces font doivent être également defnie sur tailwind.config.js pour la compilation du css-->
+<style>
+    @font-face {  font-family: unbounded;  src:url("../dev/assets/webfonts/Unbounded-Regular.ttf");  }
+    @font-face {  font-family: montserrat;  src:url("../dev/assets/webfonts/Montserrat-Regular.ttf");  }
+    @font-face {  font-family: paytone;  src:url("../dev/assets/webfonts/PaytoneOne-Regular.ttf");  }
+    @font-face {  font-family: sora;  src:url("../dev/assets/webfonts/Sora-Regular.ttf");  }
+    @font-face {  font-family: outfit;  src:url("../dev/assets/webfonts/Outfit-Regular.ttf");  }
+    @font-face {  font-family: alata;  src:url("../dev/assets/webfonts/Alata-Regular.ttf");  }
+    @font-face {  font-family: titan;  src:url("../dev/assets/webfonts/TitanOne-Regular.ttf");  }
+    @font-face {  font-family: pressstart;  src:url("../dev/assets/webfonts/PressStart2P-Regular.ttf");  }
+</style>
+
+<!--bg-white text-gray-600 dark:text-gray-400 dark:bg-gray-700 est une option pour le dark / Light-->
+<!--font-montserrat == montserrat peut être une valeur php dynamique comme option utilisateur-->
+<body class="bg-white text-gray-600 dark:text-gray-400 dark:bg-gray-700 font-montserrat">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './dev/**/*.{php,html,js}'
+    './dev/**/*.{php,html,js}',
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'unbounded': ['unbounded'],
+        'montserrat': ['montserrat'],
+        'paytone': ['paytone'],
+        'sora': ['sora'],
+        'outfit': ['outfit'],
+        'alata': ['alata'],
+        'titan': ['titan'],
+        'pressstart': ['pressstart'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
